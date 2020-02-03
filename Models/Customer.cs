af4cb659653f256dde1e0960e3cf8a4ddbb2dcbf
Riley Doll <rd484215@ohio.edu>
2020-02-03 10:34:16 -0500

    new

---
+++ Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mis42p2rd484215.Models
+{
+    public class Customer
+    {
+        public int customerId { get; set; }
+        public string customerLastName { get; set; }
+        public string customerFirstName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime customerSince { get; set; }
+
+    }
+}
---
+++ Models/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mis42p2rd484215.Models
+{
+    public class Orders
+    {
+        [Key]
+        public int orderNum { get; set; }
+        public string description { get; set; }
+        public DateTime orderDate { get; set; }
+        public int customerId { get; set; }
+        public virtual Customer Customer { get; set; }
+
+    }
+}
