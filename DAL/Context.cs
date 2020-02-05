f3996fbd126158d66b5a55489f77495852018ac1
Riley Doll <rd484215@ohio.edu>
2020-02-05 10:00:44 -0500

    dal

---
+++ DAL/Context.cs
@@ -0,0 +1,25 @@
+﻿using mis42p2rd484215.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mis42p2rd484215.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+
+        public DbSet<Products> Products { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+
+    }
+}
---
+++ Models/OrderDetail.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mis42p2rd484215.Models
+{
+    public class OrderDetail
+    {
+    }
+}
